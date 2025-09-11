9650b4cebf66760e63117d36685c469f6fc373a9
calin2406 <calinmatei20000@gmail.com>
2025-09-11 16:54:35 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>🚀 My Vitality Coach is Live!</h1>
+    </div>
+  );
+}
